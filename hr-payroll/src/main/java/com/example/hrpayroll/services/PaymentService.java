5eda8c48d06e32616a54cdecec1e59cc0c7998cb
DRLimana <daniellimana@gmail.com>
2021-02-15 14:52:38 -0300

    Feign Client restructuring

--- hr-payroll/src/main/java/com/example/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/example/hrpayroll/services/PaymentService.java
@@ -11,9 +11,14 @@ import com.example.hrpayroll.feignclients.WorkerFeignClient;
 @Service
 public class PaymentService {
 	
+	//faz a conexão entre dois ou mais projetos existentes
 	@Autowired
 	private WorkerFeignClient workerFeignClient;
 	
+	/*
+	 pega o pagamento construido no hr-payroll e busca através do feignclient o id e nome do trabalhador
+	 	
+	*/
 	public Payment getPayment(long workerId, int days) {
 		
 		Worker worker = workerFeignClient.findById(workerId).getBody(); 
